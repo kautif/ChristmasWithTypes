75fe735c4079678f97abcc51366c7ec2c242e186
Autif Kamal <kautif@gmail.com>
2023-03-28 18:38:06 -0500

    updated day and name values

--- ChristmasWithTypes/Christmas.cs
+++ ChristmasWithTypes/Christmas.cs
@@ -5,10 +5,20 @@ namespace ChristmasWithTypes
     {
         public string[] Presents;
         public string Santa { get; set; }
-        public int TreeHeight { get; set; } //TODO Make the Height property nullable
+        public int? TreeHeight { get; set; } //TODO Make the Height property nullable
 
         //TODO Make the property, "Day", type enum
-        public enum Day { /*Fill out the days of the week*/ };
+        public enum Day { 
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        };
 
     }
 }
+
+// Project fails when i run dotnet run. I think steps are missing in this exercise. Ask about it in office hours
